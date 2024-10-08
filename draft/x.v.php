767975720fafb13b86d8cffeb59183afec566081
Adilet <adiletkanybekovic03@gmail.com>
2024-10-08 23:42:25 +0200

    feat: new

---
+++ draft/x.v.php
@@ -0,0 +1,18 @@
+<?php 
+
+$footballer = [ 
+    ["name" => "C Ronaldo", "age" => 39, "club" => "Al Nasr", "national" => "Portugal"],
+    ["name" => "L Messi", "age" => 36, "club" => "Inter Maiami", "national" => "Argentina"],
+    ["name" => "Neymar", "age" => 34, "club" => "Arabia", "national" => "Brazil"],
+    ["name" => "Holland", "age" => 23, "club" => "Man City", "national" => "Norway"],
+    ["name" => "Mbappe", "age" => 25, "club" => "Real Madrid", "national" => "France"],
+];
+
+function f($footballer): mixed{
+    return $footballer;
+
+}
+
+// require "x.php";
+
+?>
