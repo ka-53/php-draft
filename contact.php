355c7ab4c473df78d389e6c262daca1977147759
Adilet <adiletkanybekovic03@gmail.com>
2024-10-13 22:06:19 +0200

    feat: add banner,footer,head

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require "contact.view.php";
+$home = 'Contact test ';
+require "views/contact.view.php";
---
+++ views/partials/nav.php
@@ -0,0 +1,20 @@
+<nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img class="h-8 w-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-10 flex items-baseline space-x-4">
+              <a href="index.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+              <a href="about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About</a>
+              <a href="contact.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Contact</a>
+              
+            </div>
+          </div>
+        </div>
+        <!-- Остальной код навигации -->
+      </div>
+    </div>
+  
