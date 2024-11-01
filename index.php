d9fa33816d49ccebca7496d7ce2abf837e113ec8
Adilet <adiletkanybekovic03@gmail.com>
2024-11-01 21:11:08 +0100

    refactor:

--- index.php
+++ index.php
@@ -2,20 +2,37 @@
 require 'functions.php';
 
 // Убираем слэши в начале и конце строки, чтобы сопоставить маршруты
-$a = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+// $a = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
-$routes = [
-    '/draft' => 'controllers/index.php',
-    '/draft/index' => 'controllers/index.php',
-    '/draft/about' => 'controllers/about.php',
-    '/draft/contact' => 'controllers/contact.php'
-];
+// $routes = [
+//     '/draft' => 'controllers/index.php',
+//     '/draft/index' => 'controllers/index.php',
+//     '/draft/about' => 'controllers/about.php',
+//     '/draft/contact' => 'controllers/contact.php'
+// ];
 
 
-if (array_key_exists($a, $routes)) {
-    require $routes[$a];
-} else {
-   http_response_code(404);
-   require 'views/404.php';
-   die();
+// if (array_key_exists($a, $routes)) {
+//     require $routes[$a];
+// } else {
+//    http_response_code(404);
+//    require 'views/404.php';
+//    die();
+// }
+
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe(){
+        echo "breathing";
+    }
 }
+
+$p = new Person();
+
+$p->name = 'Adilet';
+$p->age = 21;
+
+dd($p->name);
